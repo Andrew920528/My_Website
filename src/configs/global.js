6020444a18d9aeae9570d18c1727a1ad9fcb1581
Andrew920528 <andrewhsu.0528@gmail.com>
2023-12-27 10:20:00 +0800

    set up basic folder structure

---
+++ src/configs/global.js
@@ -0,0 +1,2 @@
+// Global variable in form of `export const var_name`
+// e.g: export const NAME = "Andrew";
---
+++ src/style/base/MuiTheme.js
@@ -0,0 +1,13 @@
+import {createTheme} from "@mui/material/styles";
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#499e93",
+    },
+    secondary: {main: "#0a183a"},
+    warning: {main: "#e5383b"},
+  },
+  typography: {
+    fontFamily: "Montserrat",
+  },
+});
