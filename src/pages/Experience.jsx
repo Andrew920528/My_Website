8bc9c43c55f1686171c3d8abd0ca33860d777fe6
Andrew920528 <andrewhsu.0528@gmail.com>
2024-01-15 17:59:07 -0500

    experience page done

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -5,31 +5,73 @@ import InfoCard from "../components/InfoCard";
 import placeholder from "../images/placeholder.jpg";
 import {ExperienceObj} from "../models/ExperienceObj";
 import {format} from "date-fns";
-const Experience = () => {
-  const nexuni = new ExperienceObj({
-    company: "Nexuni Co. Ltd.",
-    start: new Date(2023, 4),
-    end: new Date(2023, 7),
-    role: "Full Stack Software Developer",
-    description: `Working on an agile dev team, we built an image classification app
-          that identify car parts and improved its accuracy to about 75%. I
-          gained hands-on experience with using Google Cloud APIs and
-          various software for image classification such as OpenCV and
-          tensorflow.`,
-    skills: [
-      "React",
-      "HTML",
-      "JavaScript",
-      "SASS",
-      "PostgreSQL",
-      "Postman",
-      "Python",
-      "API Development",
-    ],
-    site: "https://www.nexuni.com/MainPage",
-    image: placeholder,
-  });
+import coc_logo from "../images/coc.png";
+import gpc_logo from "../images/gpc.png";
+import nex_logo from "../images/nexuni.png";
 
+const nexuni = new ExperienceObj({
+  company: "Nexuni Co. Ltd.",
+  start: new Date(2023, 4),
+  end: new Date(2023, 7),
+  role: "Full Stack Software Developer",
+  description: `During the summer internship at Nexuni, 
+                I worked on managing and visualizing data of 
+                one of the company’s biggest IOT solution.
+                My work spanned from designing and implementing 
+                interactive GUI to organizing data collected from 
+                sensor and API development. `,
+  skills: [
+    "React",
+    "HTML",
+    "JavaScript",
+    "SASS",
+    "PostgreSQL",
+    "Postman",
+    "Python",
+    "API Development",
+  ],
+  site: "https://www.nexuni.com/MainPage",
+  image: nex_logo,
+});
+
+const gpc = new ExperienceObj({
+  company: "Genuine Parts Company",
+  start: new Date(2022, 4),
+  end: new Date(2022, 7),
+  role: "Technology Intern",
+  description: `Working on an agile dev team, we built an image classification app
+        that identify car parts and improved its accuracy to about 75%. I
+        gained hands-on experience with using Google Cloud APIs and
+        various software for image classification such as OpenCV and
+        tensorflow.`,
+  skills: [
+    "Java",
+    "Spingboot",
+    "JavaScript",
+    "Google Cloud Platform",
+    "OpenCV",
+    "Python",
+    "Tensorflow",
+    "Android Studio",
+    "Git",
+  ],
+  site: "https://www.genpt.com/",
+  image: gpc_logo,
+});
+
+const gtcoc = new ExperienceObj({
+  company: "GT College of Computing",
+  start: new Date(2022, 0),
+  role: "Teaching Assitant",
+  description: `Teaches Computer Organization Concepts, Assemble, C, and Python
+                Holds office hours and teaches lab for a total of 6 hours a week.`,
+  skills: ["C", "Assembly", "Docker", "Python", "Teaching"],
+  site: "https://www.genpt.com/",
+  image: coc_logo,
+});
+
+const Experience = () => {
+  const experiences = [nexuni, gpc, gtcoc];
   return (
     <div className="experience">
       <Header title={"Experience"} />
@@ -41,19 +83,22 @@ const Experience = () => {
         <RectButton text={"View Resume"} onClick={() => {}} align={"center"} />
       </div>
       <div className="experience-cards">
-        <InfoCard
-          image={nexuni.image}
-          sub1={
-            format(nexuni.start, "yyyy MMM") +
-            " - " +
-            format(nexuni.end, "yyyy MMM")
-          }
-          title={nexuni.company}
-          sub2={nexuni.role}
-          description={nexuni.description}
-          skills={nexuni.skills}
-          site={nexuni.site}
-        />
+        {experiences.map((exp) => (
+          <InfoCard
+            image={exp.image}
+            sub1={
+              format(exp.start, "yyyy MMM") +
+              " - " +
+              (exp.end ? format(exp.end, "yyyy MMM") : "Now")
+            }
+            title={exp.company}
+            sub2={exp.role}
+            description={exp.description}
+            skills={exp.skills}
+            site={exp.site}
+            key={exp.company}
+          />
+        ))}
       </div>
     </div>
   );
