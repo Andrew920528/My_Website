74c58625a87a56f49563bdddca0c4fc23ec8959c
Andrew920528 <andrewhsu.0528@gmail.com>
2024-01-22 15:06:56 -0500

    navbar basic look done

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,4 +1,3 @@
-import {Box, Tab, Tabs} from "@mui/material";
 import React from "react";
 import {useState} from "react";
 import SpeakerNotesRoundedIcon from "@mui/icons-material/SpeakerNotesRounded";
@@ -6,52 +5,77 @@ import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
 import DocumentScannerRoundedIcon from "@mui/icons-material/DocumentScannerRounded";
 import FolderCopyRoundedIcon from "@mui/icons-material/FolderCopyRounded";
 import SendRoundedIcon from "@mui/icons-material/SendRounded";
-
-const SectionLabel = ({icon, text, selected}) => {
+import variables from "../style/abstract/_variable.scss";
+import {useTheme} from "@emotion/react";
+const SectionLabel = ({icon, text, onClick, selected}) => {
   return (
-    <div className="section-label">
+    <div className="section-label" onClick={onClick}>
       <svg width={0} height={0}>
         <linearGradient id="linearColors" x1={1} y1={0} x2={1} y2={1}>
-          <stop offset={0} stopColor="rgba(241,184,74,1)" />
-          <stop offset={1} stopColor="rgba(207,113,8,1)" />
+          <stop offset={0} stopColor={variables.primary_v} />
+          <stop offset={1} stopColor={variables.secondary_v} />
         </linearGradient>
       </svg>
       <div>{icon}</div>
-      <p>{text}</p>
+      <p className={selected ? "selected" : ""}>{text}</p>
     </div>
   );
 };
 
 const MobileNav = () => {
   const [navValue, setNavValue] = useState(0);
-  const handleChange = (event, newValue) => {
-    setNavValue(newValue);
-  };
-
+  const theme = useTheme();
   const gradient = "url('#linearColors')";
   const icons = [
-    <HomeRoundedIcon sx={{fill: gradient}} />,
-    <SpeakerNotesRoundedIcon sx={{fill: gradient}} />,
-    <DocumentScannerRoundedIcon sx={{fill: gradient}} />,
-    <FolderCopyRoundedIcon sx={{fill: gradient}} />,
-    <SendRoundedIcon sx={{fill: gradient}} />,
+    <HomeRoundedIcon sx={navValue == 0 ? {fill: gradient} : {}} />,
+    <SpeakerNotesRoundedIcon sx={navValue == 1 ? {fill: gradient} : {}} />,
+    <DocumentScannerRoundedIcon sx={navValue == 2 ? {fill: gradient} : {}} />,
+    <FolderCopyRoundedIcon sx={navValue == 3 ? {fill: gradient} : {}} />,
+    <SendRoundedIcon sx={navValue == 4 ? {fill: gradient} : {}} />,
   ];
 
   return (
-    <div className="mobile-nav">
-      <SectionLabel icon={icons[2]} text={"Home"} />
-      {/* <Box>
-        <Tabs
-          value={navValue}
-          onChange={handleChange}
-          aria-label="basic tabs example"
-        >
-          <Tab label="Item One" value={0}></Tab>
-          <Tab label="Item Two" value={1} />
-          <Tab label="Item Three" value={2} />
-          <div value={3}>ffefef</div>
-        </Tabs>
-      </Box> */}
+    <div className={`mobile-nav ${theme.mode}`}>
+      <SectionLabel
+        icon={icons[0]}
+        text={"Home"}
+        onClick={() => {
+          setNavValue(0);
+        }}
+        selected={navValue == 0}
+      />
+      <SectionLabel
+        icon={icons[1]}
+        text={"About"}
+        onClick={() => {
+          setNavValue(1);
+        }}
+        selected={navValue == 1}
+      />
+      <SectionLabel
+        icon={icons[2]}
+        text={"Experience"}
+        onClick={() => {
+          setNavValue(2);
+        }}
+        selected={navValue == 2}
+      />
+      <SectionLabel
+        icon={icons[3]}
+        text={"Projects"}
+        onClick={() => {
+          setNavValue(3);
+        }}
+        selected={navValue == 3}
+      />
+      <SectionLabel
+        icon={icons[4]}
+        text={"Contact"}
+        onClick={() => {
+          setNavValue(4);
+        }}
+        selected={navValue == 4}
+      />
     </div>
   );
 };
